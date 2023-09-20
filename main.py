576736621f113f5f2d78a1f1113759bcb3c7c8a3
chaudharyvikash <130667534+chaudharyvikash@users.noreply.github.com>
2023-09-20 18:20:57 +0530

    Update main.py
    write date time

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import platform as os
+
 import os
 from datetime import datetime
 
@@ -11,20 +11,4 @@ file_name_2 = x.strftime('%d-%m-%Y-%H-%M-%S.txt')
 with open("output.json", 'w') as fp:
     fp.write(file_name_2)
 
-print('Operating system name: ',os.system())
-print('Host name: ',os.node())
-print('Machine name: ',os.machine())
-print('Processor name: ',os.processor())
-print('Release: ',os.release())
-print('Version: ',os.version())
-
-#second method
-
-info=os.uname()     #uname returns tuple of string
-print('using tuple')
-print('Operating system name: ',info.system)
-print('Host name: ',info.node)
-print('Machine name: ',info.machine)
-print('Processor name: ',info.processor)
-print('OS Release: ',info.release)
-print('OS Version: ',info.version)
+fp.close()
