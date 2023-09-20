bd6ec1c7dc78877d86a962ec806767f45404dafa
chaudharyvikash <130667534+chaudharyvikash@users.noreply.github.com>
2023-09-20 17:23:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 import platform as os
+import os
+from datetime import datetime
+
+# get current date and time
+x = datetime.now()
+
+
+# with name as day-month-year-hours-minutes-seconds
+file_name_2 = x.strftime('%d-%m-%Y-%H-%M-%S.txt')
+with open("output.json", 'w') as fp:
+    fp.write(file_name_2)
 
 print('Operating system name: ',os.system())
 print('Host name: ',os.node())
