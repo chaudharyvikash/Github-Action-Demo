91bae4e3c85003aced28580b57af49584a5aa079
chaudharyvikash <130667534+chaudharyvikash@users.noreply.github.com>
2023-09-22 19:05:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 import os
 from datetime import datetime
-import requests
+#import requests
 
 # get current date & time
 x = datetime.now()
@@ -13,21 +13,3 @@ with open("output.json", 'w') as fp:
     fp.write(var)
 
 
-# %%
-owner = "chaudharyvikash"
-repo = "Github-Action-Demo"
-param = {"name":"output"}
-
-headers = {
-
-    "Accept": "application/vnd.github+json",
-    "X-GitHub-Api-Version": "2022-11-28"
-
-}
-
-res = requests.get(
-
-    f"https://api.github.com/repos/{owner}/{repo}/actions/artifacts", headers=headers, params=param)
-artifacts = res.json()
-
-print(artifacts)
