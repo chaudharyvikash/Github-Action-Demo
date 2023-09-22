7ec89d4266c3eb95cdc9ebcc712b4418d46a42cc
chaudharyvikash <130667534+chaudharyvikash@users.noreply.github.com>
2023-09-22 16:51:11 +0530

    Update write.py

--- write.py
+++ write.py
@@ -3,11 +3,11 @@ import os
 # get current date and time
 x = datetime.now()
 
-# with name as day-month-year-hours-minutes-seconds
+
 
 file_name= "result.json"
 path = 'C:\Projects\kev_cve_import'
-
+# with name as day-month-year-hours-minutes-seconds
 file = x.strftime('%d-%m-%Y-%H-%M-%S')
 output_file = os.path.join(path, file_name)
 with open("output_file", "w") as json_file:
